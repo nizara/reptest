feaf09a62598353e82d73b4e3d5dc6a00ab318a3
n.ajroud <n.ajroud@AjroudN-Dell.sofrecom-tunisie.com>
2015-08-09 20:27:50 +0100

    ini

---
+++ test/src/test/pkg/main/MainClass.java
@@ -0,0 +1,16 @@
+package test.pkg.main;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		SecClass sec = new SecClass();
+		sec.displayAnyThing();
+//		System.out.println("b1c1");
+//		System.out.println("b1c2");
+//		System.out.println("b2c1");
+//		System.out.println("b2c2");
+//		System.out.println("b3c1");
+//		System.out.println("b3c2");
+	}
+
+}
---
+++ test/src/test/pkg/main/SecClass.java
@@ -0,0 +1,10 @@
+package test.pkg.main;
+
+public class SecClass {
+	
+	public void displayAnyThing()
+	{
+		System.out.println("any thing");
+	}
+
+}
